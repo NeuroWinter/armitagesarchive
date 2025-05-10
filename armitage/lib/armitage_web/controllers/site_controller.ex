a40c5739e3560aeb2fd8d0bec2f0b44d52b678fa
NeuroWinter <alex@pikori.com>
2025-05-10 22:34:01 +1200

    feat(sitemap): add highlight permalink URLs for books and articles

--- armitage/lib/armitage_web/controllers/site_controller.ex
+++ armitage/lib/armitage_web/controllers/site_controller.ex
@@ -24,18 +24,38 @@ defmodule ArmitageWeb.SiteController do
         }
       end)
 
-    urls =
+    highlights =
+      from(h in Armitage.Highlight,
+        join: b in Armitage.Book,
+        on: h.book_id == b.id,
+        where: not is_nil(h.slug) and not is_nil(b.slug),
+        select: {b.category, b.slug, h.slug}
+      )
+      |> Repo.all()
+      |> Enum.map(fn
+        {"books", book_slug, highlight_slug} ->
+          url(conn, ~p"/books/#{book_slug}/#{highlight_slug}")
+
+        {"articles", article_slug, highlight_slug} ->
+          url(conn, ~p"/articles/#{article_slug}/#{highlight_slug}")
+      end)
+
+    static_urls =
       [
         url(conn, ~p"/"),
         url(conn, ~p"/books"),
         url(conn, ~p"/articles"),
         url(conn, ~p"/readwise"),
         url(conn, ~p"/highlights")
-      ] ++
-        Enum.map(books, & &1.loc) ++
-        Enum.map(articles, & &1.loc)
+      ]
 
-    xml = build_sitemap(urls)
+    all_urls =
+      Enum.map(books, & &1.loc) ++
+      Enum.map(articles, & &1.loc) ++
+      highlights ++
+      static_urls
+
+    xml = build_sitemap(all_urls)
 
     conn
     |> put_resp_content_type("application/xml")
